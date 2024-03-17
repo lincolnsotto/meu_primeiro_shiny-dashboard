9dd22861d80e73d38ce3f1537f9450e9f013dd27
Lincoln Sotto <lincoln.sotto@hotmail.com>
2024-03-17 12:35:06 -0300

    20240317 upload dos arquivos do projeto

---
+++ my_app_2/app2.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+# Definindo a UI
+ui <- fluidPage(
+   titlePanel("censuVis"),
+   
+   
+   sidebarLayout(
+     
+      sidebarPanel(
+        
+        helpText("Gráfico  com informações do Censo 2010"),
+        
+        selectInput(
+                    "variavel", h3("Escolha a variável para mostrar:"), 
+                    choices = list("Escolha 1" = 1, 
+                                   "Escolha 2" = 2, 
+                                   "Escolha 3" = 3),
+                                   selected = 1),
+        
+        sliderInput("range", label = ("Escolha o intervalo:"), 
+                    min = 0, 
+                    max = 100, 
+                    value = c(0, 100)),
+      ),
+      
+      mainPanel(
+        textOutput("var_selecionada"),
+      )
+    )
+  )
+
+
+# Definindo lógica do servidor
+server <- function(input,output){
+  output$var_selecionada <- renderText({
+    paste("Você escolheu a variável", input$variavel)
+  })
+}
+
+# Carregando o aplicativo
+shinyApp(ui = ui, server = server)
+
